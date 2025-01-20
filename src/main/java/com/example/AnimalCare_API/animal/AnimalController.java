0192939fcd3a945184f1d16fefa84bae71bf7217
Andreea <andreea.celmare@gmail.com>
2025-01-20 15:12:37 +0100

    feat: CRUD Post - Create Animal, works correctly in Postman & DB

--- src/main/java/com/example/AnimalCare_API/animal/AnimalController.java
+++ src/main/java/com/example/AnimalCare_API/animal/AnimalController.java
@@ -1,9 +1,12 @@
 package com.example.AnimalCare_API.animal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping ("/api/admin/animals")
@@ -20,4 +23,21 @@ public class AnimalController {
         return this.animalRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Animal> getAnimalsByID(@PathVariable Long id) {
+        Optional<Animal> optionalAnimal = animalRepository.findById(id);
+
+        if (optionalAnimal.isPresent()) {
+            return new ResponseEntity<>(optionalAnimal.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public Animal createAnimal (@RequestBody Animal animal) {
+        Animal savedAnimal = animalRepository.save(animal);
+        return savedAnimal;
+
+
+    }
 }
