8f61d74e22f79b35fdf6992d05a7c27c051f70eb
Andreea <andreea.celmare@gmail.com>
2025-01-20 10:02:19 +0100

    first commit

---
+++ src/test/java/com/example/AnimalCare_API/AnimalCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnimalCare_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
